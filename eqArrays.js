5073268ca8edcc9a24a4040cc5e2fda48b28b96e
Robert Shum <robertshum@gmail.com>
2023-09-22 18:10:33 -0700

    finished eqArrays recurisve, trying to fix eqObjects.

--- eqArrays.js
+++ eqArrays.js
@@ -16,7 +16,10 @@ const eqArrays = function(array1, array2) {
 
   //loop and check each element
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
+
+    if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+      eqArrays(array1[i], array2[i]);
+    } else if (array1[i] !== array2[i]) {
       //if any fail, just return false;
       return false;
     }
@@ -32,3 +35,10 @@ assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should FAIL
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should FAIL
+
+
+//Test Cases for Recursion
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+assertEqual(eqArrays([[2, 3], [4]], [2, 3], [4, 5]), false); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
+assertEqual(eqArrays([[2, 3], [4, [99,98]]], [[2, 3], [4, [99,98]]]), true); // => false
--- eqObjects.js
+++ eqObjects.js
@@ -7,8 +7,57 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true); // 
+
 // Implement the definition for function eqObjects which will take in two objects and returns true or false, based on a perfect match.
 const eqObjects = function(objOne, objTwo) {
+
+  let result = true;
+
+  //if one of the values is undefined
+  if(objOne === undefined || objTwo === undefined) {
+    return false;
+  }
+
+
+
+  //check if they have the same amount of keys.  If not, just return false
+  //...probably should check the value too lol but i got lazy
+  const keys1 = Object.keys(objOne);
+  const keys2 = Object.keys(objTwo);
+  if (keys1.length !== keys2.length) {
+    result = false;
+  }
+
+  //navigate objOne with for ... in loop
+
+  //they are equal if:
+  //1 They have the same number of keys
+  //2 The value for each key in one object is the same as the value for that same key in the other object
+  for (let key in objOne) {
+    const value1 = objOne[key];
+    const value2 = objTwo[key];
+    if (Array.isArray(value1) && Array.isArray(value2)) {
+      const arrayMatches = eqArrays(value1, value2);
+      if (!arrayMatches) {
+        result = false;
+      }
+      //both objects are not arrays BUT still objects
+    } else if (!Array.isArray(value1) && !Array.isArray(value2)) {
+      result = eqObjects(value1, value2);
+    }
+  }
+  
+  if (objOne !== objTwo) {
+    result =  false;
+  }
+
+  //if at this point we just return true;
+  return result;
+};
+
+// Implement the definition for function eqObjects which will take in two objects and returns true or false, based on a perfect match.
+const eqObjectsNonRecursive = function(objOne, objTwo) {
 
   //check if they have the same amount of keys.  If not, just return false
   const keys1 = Object.keys(objOne);
@@ -30,7 +79,8 @@ const eqObjects = function(objOne, objTwo) {
       if (!arrayMatches) {
         return false;
       }
-    } else if (value1 !== value2) {
+    } 
+    else if (value1 !== value2) {
       return false;
     }
   }
@@ -58,6 +108,18 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
+//Tests for Recursive Objects
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true); // => true
+
+// assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false); // => false
+// assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false); // => false
+
+// assertEqual(eqObjects({ a: { y: 0, z: 1, x : {mom: 1, dad: 1}}, b: 2 }, { a: { y: 0, z: 1, x : {mom: 1, dad: 1}}, b: 2 }), true); // => true
+
+// assertEqual(eqObjects({ a: { y: 0, z: 1, x : {mom: 1, dad: 1}}, b: 2 }, { a: { y: 0, z: 1, x : {mom: 1}}, b: 2 }), false); // => false
+
+return;
+
 //Tests
 const shirtObject = { color: "red", size: "medium" };
 const anotherShirtObject = { size: "medium", color: "red" };
@@ -74,3 +136,4 @@ assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true
 
 const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
 assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false); // => false
+
