b72d30695b08e049cf62740313382bd3557462f8
Robert Shum <robertshum@gmail.com>
2023-09-23 13:50:31 -0700

    finally fixed eqObjects recursivegit add .!

--- eqObjects.js
+++ eqObjects.js
@@ -7,53 +7,49 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true); // 
+// { a: { y: 0, z: 1, x : {mom: 1, dad: 1}}, b: 2 }, 
+// { a: { y: 0, z: 1, x : {mom: 1}}, b: 2 } // TRUE 
 
 // Implement the definition for function eqObjects which will take in two objects and returns true or false, based on a perfect match.
 const eqObjects = function(objOne, objTwo) {
 
-  let result = true;
-
-  //if one of the values is undefined
-  if(objOne === undefined || objTwo === undefined) {
+  //check if both object matches in type?
+  if (typeof objOne !== typeof objTwo) {
     return false;
   }
 
+  //base case - if both values are arrays
+  if (Array.isArray(objOne) && Array.isArray(objTwo)) {
+    return eqArrays(objOne, objTwo);
+  }
 
+  //--here and below - are the same, and not arrays--
 
   //check if they have the same amount of keys.  If not, just return false
-  //...probably should check the value too lol but i got lazy
   const keys1 = Object.keys(objOne);
   const keys2 = Object.keys(objTwo);
   if (keys1.length !== keys2.length) {
-    result = false;
+    return false;
   }
 
-  //navigate objOne with for ... in loop
-
-  //they are equal if:
-  //1 They have the same number of keys
-  //2 The value for each key in one object is the same as the value for that same key in the other object
-  for (let key in objOne) {
-    const value1 = objOne[key];
-    const value2 = objTwo[key];
-    if (Array.isArray(value1) && Array.isArray(value2)) {
-      const arrayMatches = eqArrays(value1, value2);
-      if (!arrayMatches) {
-        result = false;
+  if (typeof objOne === "object") {
+    //safely assume objTwo is also object because they would fail up above
+    for (let key in objOne) {
+      //we only care for a single instance of false
+      //thats why we don't care for TRUTH values here:
+      if (!eqObjects(objOne[key], objTwo[key])) {
+        return false;
       }
-      //both objects are not arrays BUT still objects
-    } else if (!Array.isArray(value1) && !Array.isArray(value2)) {
-      result = eqObjects(value1, value2);
     }
   }
-  
-  if (objOne !== objTwo) {
-    result =  false;
+
+  //make sure they are not objects
+  if (typeof objOne !== "object" && typeof obj2 !== "object") {
+    return objOne === objTwo; //if this returns false, the callstack will close and return false.  if it's true then the loop from above continues (from the previous stack).
   }
 
-  //if at this point we just return true;
-  return result;
+  //it's true - run until list exhausts
+  return true;
 };
 
 // Implement the definition for function eqObjects which will take in two objects and returns true or false, based on a perfect match.
@@ -79,7 +75,7 @@ const eqObjectsNonRecursive = function(objOne, objTwo) {
       if (!arrayMatches) {
         return false;
       }
-    } 
+    }
     else if (value1 !== value2) {
       return false;
     }
@@ -109,16 +105,18 @@ const eqArrays = function(array1, array2) {
 };
 
 //Tests for Recursive Objects
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 20 }), false); // => false
+
 assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true); // => true
 
-// assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false); // => false
-// assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false); // => false
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false); // => false
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false); // => false
 
-// assertEqual(eqObjects({ a: { y: 0, z: 1, x : {mom: 1, dad: 1}}, b: 2 }, { a: { y: 0, z: 1, x : {mom: 1, dad: 1}}, b: 2 }), true); // => true
+assertEqual(eqObjects({ a: { y: 0, z: 1, x: { mom: 1, dad: 1 } }, b: 2 }, { a: { y: 0, z: 1, x: { mom: 1, dad: 1 } }, b: 2 }), true); // => true
 
-// assertEqual(eqObjects({ a: { y: 0, z: 1, x : {mom: 1, dad: 1}}, b: 2 }, { a: { y: 0, z: 1, x : {mom: 1}}, b: 2 }), false); // => false
+assertEqual(eqObjects({ a: { y: 0, z: 1, x: { mom: 1, dad: 1 } }, b: 2 }, { a: { y: 0, z: 1, x: { mom: 1 } }, b: 2 }), false); // => false
 
-return;
 
 //Tests
 const shirtObject = { color: "red", size: "medium" };
