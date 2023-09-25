9eca9ad1ea72659610b1144d00e0e370cbe566ac
Robert Shum <robertshum@gmail.com>
2023-09-25 11:01:07 -0700

    refactored index to use shorthand for exports

--- index.js
+++ index.js
@@ -6,11 +6,22 @@ const assertArraysEqual = require("./assertArraysEqual");
 const assertEqual = require("./assertEqual");
 
 //Missing some methods that we did not refactor such as flatten, without, countLetters etc.
+
+//we can use the shorthand for obj. literals.  Values don't need to be explicit if the keys match the variable names.
+// module.exports = {
+//   head: head,
+//   tail: tail,
+//   middle: middle,
+//   eqArrays: eqArrays,
+//   assertArraysEqual: assertArraysEqual,
+//   assertEqual : assertEqual
+// };
+
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  eqArrays: eqArrays,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual : assertEqual
+  head,
+  tail,
+  middle,
+  eqArrays,
+  assertArraysEqual,
+  assertEqual
 };
