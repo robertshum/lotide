388ea109b176e55b471025b4656807d1e5f6154c
Robert Shum <robertshum@gmail.com>
2023-09-24 23:11:34 -0700

    forgot to remove assertEq from tail.  Fixed eqArrays for DRYness.

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  //use string literals ` for performance gains! (ie: don't use '+').
-  if (actual === expected) {
-    console.log(`✔ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
 
   //check if array sizes match
@@ -29,16 +20,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-//Test Cases
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should FAIL
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should FAIL
-
-
-//Test Cases for Recursion
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
-assertEqual(eqArrays([[2, 3], [4]], [2, 3], [4, 5]), false); // => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
-assertEqual(eqArrays([[2, 3], [4, [99,98]]], [[2, 3], [4, [99,98]]]), true); // => false
+module.exports = eqArrays;
--- tail.js
+++ tail.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  //use string literals ` for performance gains! (ie: don't use '+').
-  if (actual === expected) {
-    console.log(`✔ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   if (array.length === 1) {
     return [];
